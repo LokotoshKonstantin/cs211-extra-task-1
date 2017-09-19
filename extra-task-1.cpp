34dda41523962c590dd9fc64a04074a59b57ecbc
LokotoshKonstantin <Lokotoch777@mail.ru>
2017-09-19 23:58:38 +0300

    hours_difference

--- extra-task-1.cpp
+++ extra-task-1.cpp
@@ -9,12 +9,11 @@ double seconds_difference(double time_1, double time_2)
 	
 }
 
-/*
 double hours_difference(double time_1, double time_2)
 {
 	return (time_2 - time_1) / 3600;
 }
-
+/*
 double to_float_hours(int hours, int minutes, int seconds)
 {
 	assert(minutes >= 0);
